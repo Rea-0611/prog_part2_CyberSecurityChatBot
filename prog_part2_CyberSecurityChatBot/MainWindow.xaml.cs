8fa69e54006a9051e74bc11a700d6cd53623aea2
Rea-0611 <st10483202@rcconnect.edu.za>
2026-05-29 10:19:40 +0200

    Revert "User interface design added in the MainWindow.xaml. Calling of external classes added in order to be able to create the external classes by name and order. Also added the greet file and image file to the main file."
    This reverts commit 2c171655732ce8f4023bc15abeb4c389a56a40c7.

--- prog_part2_CyberSecurityChatBot/MainWindow.xaml.cs
+++ prog_part2_CyberSecurityChatBot/MainWindow.xaml.cs
@@ -18,107 +18,11 @@ namespace prog_part2_CyberSecurityChatBot
 
     public partial class MainWindow : Window
     {//start of class
+        public MainWindow()
+        {//start of constructor
+            InitializeComponent();
 
-     
-       
-            // All managers
-            private GreetingManager greetingManager;
-            private NavigationManager navigationManager;
-            private UsernameManager usernameManager;
-            private ResponseManager responseManager;
-            private UIManager uiManager;
-            private ChatManager chatManager;
 
-            public MainWindow()
-            {
-                InitializeComponent();
-
-                // Initialize all managers
-                InitializeManagers();
-
-                // Play voice greeting
-                greetingManager.PlayVoiceGreeting();
-            }
-
-            private void InitializeManagers()
-            {
-                // Create UI Manager first (handles visual updates)
-                uiManager = new UIManager();
-
-                // Create other managers
-                greetingManager = new GreetingManager();
-                navigationManager = new NavigationManager(home_grid, username_grid, chat_grid);
-                usernameManager = new UsernameManager();
-                responseManager = new ResponseManager();
-                chatManager = new ChatManager(usernameManager, responseManager, uiManager);
-            }
-
-            // Start button click - navigates to username page
-            private void welcome(object sender, RoutedEventArgs e)
-            {
-                navigationManager.ShowUsernamePage();
-                usernames_input.Focus();
-            }
-
-            // Enter key in username textbox
-            private void UsernameTextBox_KeyDown(object sender, KeyEventArgs e)
-            {
-                if (e.Key == Key.Enter)
-                {
-                    SubmitUsername();
-                    e.Handled = true;
-                }
-            }
-
-            // Submit username button click
-            private void submit_name(object sender, RoutedEventArgs e)
-            {
-                SubmitUsername();
-            }
-
-            private void SubmitUsername()
-            {
-                string enteredName = usernames_input.Text.Trim();
-                string welcomeMessage;
-                bool isNewUser;
-
-                if (usernameManager.ProcessUsername(enteredName, out welcomeMessage, out isNewUser))
-                {
-                    uiManager.ShowUsernameError(error_message, false);
-                    uiManager.UpdateUserGreeting(user_greeting, usernameManager.CurrentUsername);
-
-                    uiManager.AddBotMessage(welcomeMessage, chats);
-                    chatManager.SendWelcomeMessages(chats);
-
-                    navigationManager.ShowChatPage();
-                    uiManager.FocusInput(question);
-                }
-                else
-                {
-                    uiManager.ShowUsernameError(error_message, true);
-                }
-            }
-
-            // Enter key in question textbox
-            private void Question_KeyDown(object sender, KeyEventArgs e)
-            {
-                if (e.Key == Key.Enter)
-                {
-                    SendQuestion();
-                    e.Handled = true;
-                }
-            }
-
-            // Send button click
-            private void send(object sender, RoutedEventArgs e)
-            {
-                SendQuestion();
-            }
-
-            private void SendQuestion()
-            {
-                chatManager.ProcessUserQuestion(question.Text, chats, question);
-            }
-        }
-    }
-    
+        }//end of constructor
+    }//end of class
+}//end of namespace
