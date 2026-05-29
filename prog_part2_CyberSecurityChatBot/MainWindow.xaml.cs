296b6c1e60c1951e2d10a449c1c8c551d986538e
Rea-0611 <st10483202@rcconnect.edu.za>
2026-05-29 09:56:04 +0200

    Add project files.

---
+++ prog_part2_CyberSecurityChatBot/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prog_part2_CyberSecurityChatBot
+{//start of namespace
+
+    public partial class MainWindow : Window
+    {//start of class
+        public MainWindow()
+        {//start of constructor
+            InitializeComponent();
+
+
+        }//end of constructor
+    }//end of class
+}//end of namespace
